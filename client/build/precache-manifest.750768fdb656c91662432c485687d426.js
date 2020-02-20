6e5e6aec2fd6c7c6759a8f54d7d74fcb9bbde198
Cole Snyder <csnyder14@winona.edu>
2020-02-20 16:16:56 -0600

    setting up heroku

---
+++ client/build/precache-manifest.750768fdb656c91662432c485687d426.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d0b917d69e9c8583cec8153adffb9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f079b40d50f3b3047771",
+    "url": "/static/css/2.443f96f2.chunk.css"
+  },
+  {
+    "revision": "ea0724c4a00e723fdafd",
+    "url": "/static/css/main.6c63d624.chunk.css"
+  },
+  {
+    "revision": "f079b40d50f3b3047771",
+    "url": "/static/js/2.e9d16a30.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.e9d16a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0724c4a00e723fdafd",
+    "url": "/static/js/main.97ce3920.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b4b34dbef059c7481a26a2f8bea8375b",
+    "url": "/static/media/warehouse.b4b34dbe.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 var bodyParser = require('body-parser');
 
 const app = express();
+// 1
 const PORT = process.env.PORT || 8080;
 
 // parse application/x-www-form-urlencoded
@@ -16,12 +17,18 @@ const routes = require('./routes/api');
 
 // the following line contains the URI for the database connection. In a real world, live applicaiton 
 // I would obscure these credentials for good practice. 
-const MONGODB_URI = 'mongodb+srv://ColeSnyder:Diabetic123@bincluster-y3cn9.mongodb.net/test?retryWrites=true&w=majority';
+// const MONGODB_URI = 'mongodb+srv://ColeSnyder:Diabetic123@bincluster-y3cn9.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/scout', {
+// 2
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/scout', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+// mongoose.connect(MONGODB_URI || 'mongodb://localhost/scout', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
 
 mongoose.connection.on('connected', () => {
     console.log("Connected to mongoose.");
@@ -32,5 +39,10 @@ mongoose.set('useFindAndModify', false);
 //http logger
 app.use(morgan('tiny'));
 app.use('/api', routes);
+
+// 3
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
